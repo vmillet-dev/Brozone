e9c5f7b6dfda7956678701e7bb1f7b1f188861c6
Valentin MILLET <34792012+vmillet-dev@users.noreply.github.com>
2024-01-14 14:28:59 +0100

    Update README.md and add some CI/CD config (#5)
    * [WiP] Update readme and start to add cicd file
    
    * Add jacoco report, must be configured
    
    * split README into multi markdown files and add jacoco dependency for code coverage
    
    * Test sonar config
    
    * fix github workflow
    
    * fix github workflow
    
    * add android download inside github workflow and setup java version
    
    * fix build order
    
    * Move sonar settings inside main build gradle file
    
    * Update workflow script to follow github doc
    
    * Add break line
    
    * change sonar path
    
    * Remove useless env
    
    * Remove project name sonar property
    
    * Add github envirnnement to job properties
    
    * Update actions/cache version and add caches to build job
    
    * Try plugin to have a better view of tests executed
    
    * fix typo in workflow file
    
    * remove tests from gradle build
    
    * Add property to sonar plugn for skipping compile and remove warning
    
    * Refacto gradle build properties
    
    * Fix build fail
    
    * Exclude .java files to sonar analyses
    
    * Add sonar analyze for every branches
    
    ---------
    
    Co-authored-by: VALENTIN MILLET <valentin-externe.millet@enedis.fr>

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Brozone
+
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=vmillet-dev_Brozone&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=vmillet-dev_Brozone)
+[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=vmillet-dev_Brozone&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=vmillet-dev_Brozone)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=vmillet-dev_Brozone&metric=coverage)](https://sonarcloud.io/summary/new_code?id=vmillet-dev_Brozone)
+[![Lines of Code](https://sonarcloud.io/api/project_badges/measure?project=vmillet-dev_Brozone&metric=ncloc)](https://sonarcloud.io/summary/new_code?id=vmillet-dev_Brozone)
+
+## Table of contents
+
+- [Feature](#Features)
+- [Project layout](docs/PROJECT_STRUCTURE.md)
+- [Development](docs/INSTALL.md)
+- [Tests](docs/TESTS.md)
+- [Deploying]()
+
+## Features
+
+### Gameplay
+
+- Not implemented
+
+### Environment
+
+- [x] Windows / MacOS / Android / web server support (not Linux yet)
+- [x] Support Java 17
+- [x] Auto-upload release on merge in main branch
--- core/src/main/java/dev/vmillet/brozone/Brozone.java
+++ core/src/main/java/dev/vmillet/brozone/Brozone.java
@@ -22,9 +22,6 @@ public class Brozone extends Game {
 	private GameOptions options;
 	private boolean isMobile;
 
-	/**
-	 * Method called once when the application is created.
-	 */
 	@Override
 	public void create () {
 		setDebugLevelMode();
@@ -40,10 +37,6 @@ public class Brozone extends Game {
 		inputManager.setScreen(gameManager.getScreens().getMainMenuScreen());
 	}
 
-	/**
-	 * 	Method called by the game loop from the application every time rendering should be performed.
-	 * 	Game logic updates are usually also performed in this method.
-	 */
 	@Override
 	public void render () {
 		super.render();
@@ -52,41 +45,23 @@ public class Brozone extends Game {
 		// MVP maybe must add a drawing manager to draw all objects only once
 	}
 
-	/**
-	 * This method is called every time the game screen is re-sized and the game is not in the paused state. It is also called once just after the create() method.
-	 * The parameters are the new width and height the screen has been resized to in pixels.
-	 *
-	 * @param width the new width in pixels
-	 * @param height the new height in pixels
-	 */
 	@Override
 	public void resize (int width, int height) {
 		super.resize(width, height);
 	}
 
-	/**
-	 * On Android this method is called when the Home button is pressed or an incoming call is received. On desktop this is called when the window is minimized and just before dispose() when exiting the application.
-	 * A good place to save the game state.
-	 */
 	@Override
 	public void pause () {
 		super.pause();
 		// MVP handle pause
 	}
 
-	/**
-	 * 	This method is called on Android, when the application resumes from a paused state, and on desktop when unminimized.
-	 */
 	@Override
 	public void resume () {
 		super.resume();
 		// MVP handle resume
 	}
 
-
-	/**
-	 * 	Called when the application is destroyed. It is preceded by a call to pause().
-	 */
 	@Override
 	public void dispose () {
 		batch.dispose();
